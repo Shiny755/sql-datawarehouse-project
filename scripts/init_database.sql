1392c32cf6d4280869bb28250d8f158c77904273
Shiny755 <152352899+Shiny755@users.noreply.github.com>
2026-02-09 14:10:46 +0530

    Initialize DataWarehouse database and schemas

---
+++ scripts/init_database.sql
@@ -0,0 +1,21 @@
+use master;
+
+
+if exists(select 1 from sys.databases where name='DataWarehouse')
+  begin
+      alter database DataWarehouse set single_user with rollback immediate;
+      drop database DataWarehouse;
+  end;
+go;
+
+create database DataWarehouse;
+
+use DataWarehouse;
+go;
+
+create schema bronze;
+go;
+create schema silver;
+go;
+create schema gold;
+go;
